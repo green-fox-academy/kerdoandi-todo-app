98d9072c360543b895287157ec1d8a7805e519e8
Kerdo Andrea <kerdoandi@gmail.com>
2017-04-07 10:16:03 +0200

    Add unsupported args

---
+++ src/Arguments.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Andi on 4/7/2017.
+ */
+private class Arguments {
+  List<String> argList;
+
+
+  public Arguments() {
+    argList = new ArrayList<>();
+  }
+
+  public List<String> getArgList() {
+    return argList;
+  }
+
+  public void setArgList(List<String> argList) {
+    this.argList = argList;
+  }
+
+  public void addArgs(String newArg) {
+    argList.add(newArg);
+  }
+}
+
+
+
+
+
+
+
--- src/ToDoApp.java
+++ src/ToDoApp.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+
 public class ToDoApp {
-
-
   public static void main(String[] args) {
     TaskList taskList = new TaskList();
     taskList.readAddToTaskList();
+    Arguments argList = new Arguments();
+    argList.addArgs("-l");
+    argList.addArgs("-a");
+    argList.addArgs("-c");
+    argList.addArgs("-r");
 
     if (args.length == 0) {
-      System.out.println("Python Todo application\n" + "=======================\n" + "\n" + "Command line arguments:\n" + " -l   Lists all the tasks\n" + " -a   Adds a new task\n" + " -r   Removes an task\n" + " -c   Completes an task\n");
+      System.out.println("JAVA Todo application\n" + "=======================\n" + "\n" + "Command line arguments:\n" + " -l   Lists all the tasks\n" + " -a   Adds a new task\n" + " -r   Removes an task\n" + " -c   Completes an task\n");
     } else if (args[0].equals("-l")) {
       taskList.toListContent();
     } else if (args[0].equals("-a") && args.length == 2) {
@@ -17,6 +22,8 @@ public class ToDoApp {
       taskList.getTask(Integer.valueOf(args[1]) - 1).setStatusDone();
     } else if (args[0].equals("-r") && args.length == 2) {
       taskList.removeTask(Integer.valueOf(args[1]) - 1);
+    } else if (!args[0].equals(argList)) {
+      System.out.println("Unsupported argument");
     }
     taskList.overwriteFile();
   }
